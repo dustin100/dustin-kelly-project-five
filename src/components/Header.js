002124231a51bcbfede55fb587b16f51caa7a9d2
Dustin <dmkelly100@gmail.com>
2020-06-02 15:28:04 -0400

    added toogle button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 
 const Header = ( {title, tagLine, showFavs}) => {
+
+	const [text, setText] = useState(false);
+	const toggleTrueFalse = () => setText(!text);
 	
 	return (
 		<header>
 			<div className="wrapper headContent">
 				<h1>{title}</h1>
 				<p>{tagLine}</p>
-				<div onClick= {showFavs} className="userList">
-					<i className="fas fa-th-list"></i>
+				<div onClick={showFavs} className="userList">
+					<p className="aboveBtn">Go to</p>
+					<button className="btn" onClick={toggleTrueFalse}>
+						{text ? `My List` : `Search Results`}
+					</button>
 				</div>
 			</div>
 		</header>
